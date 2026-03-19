3f41424cce94205026bb41c5d369071a34eb1b02
Zebrar <777evgeniy2301@gmail.com>
2026-03-19 21:33:15 +0500

    fix: Исправил баг, из-за которого не работала проврка на существование сцены

--- Assets/Scripts/DoorHandler.cs
+++ Assets/Scripts/DoorHandler.cs
@@ -3,20 +3,21 @@ using UnityEngine.SceneManagement;
 
 public class DoorHandler : MonoBehaviour
 {
-    
     public string toSceneName;
 
     public void Interact()
     {
         Debug.Log("Используем дверь, которая ведёт на сцену " + toSceneName);
-        if(SceneManager.GetSceneByName(toSceneName).IsValid())
+
+        int sceneIndex = SceneUtility.GetBuildIndexByScenePath(toSceneName);
+
+        if (sceneIndex != -1)
         {
             SceneManager.LoadScene(toSceneName);
-        } else
-        {
-            Debug.LogWarning("Не найдена сцена с именем " + toSceneName);
         }
-        
+        else
+        {
+            Debug.LogError($"Сцена '{toSceneName}' не найдена в Build Settings! " + "Проверьте правильность имени или добавьте сцену в окно Build Settings.");
+        }
     }
-
 }
